ca8cf7220daf306352aa67f50684cc58f194f1cf
Sheil Sarda <sheilsarda@yahoo.com>
2026-01-17 13:06:12 -0800

    cycling script works better

--- initialize_arm.py
+++ initialize_arm.py
@@ -9,34 +9,49 @@ try:
         s.connect((ROBOT_IP, DASHBOARD_SERVER_PORT))
         response = s.recv(1024).decode('utf-8')
         print(response)
+
+
         
         while(True):
-            s.sendall("power on\n".encode('utf-8'))
-            response = s.recv(1024).decode('utf-8')
-            print(response)
+            s.sendall("robotmode\n".encode('utf-8'))
+            current_status = s.recv(1024).decode('utf-8')
+            print("Current status is ", current_status)
 
-            while response.find("IDLE") == -1:
-                s.sendall("robotmode\n".encode('utf-8'))
+            if current_status.find("POWER_OFF") != -1:
+                s.sendall("power on\n".encode('utf-8'))
                 response = s.recv(1024).decode('utf-8')
+                print(response)
+
+                while current_status.find("IDLE") < 0:
+                    s.sendall("robotmode\n".encode('utf-8'))
+                    current_status = s.recv(1024).decode('utf-8')
             
-            print("Robot is powered on; status is ", response)
-        
-            s.sendall("brake release\n".encode('utf-8'))
-            response = s.recv(1024).decode('utf-8')
-            print(response)
+                print("Robot is powered on; status is ", response)
 
-            while response.find("RUNNING") == -1:
-                s.sendall("robotmode\n".encode('utf-8'))
-                response = s.recv(1024).decode('utf-8')
+            else:
+                print("Assuming robot is powered on; status is ", current_status)
         
-            print("Brakes disengaged; status is ", response)
-            time.sleep(3)
+            if current_status.find("RUNNING") == -1:
+                s.sendall("brake release\n".encode('utf-8'))
+                response = s.recv(1024).decode('utf-8')
+                print(response)
+
+                while current_status.find("RUNNING") < 0:
+                    s.sendall("robotmode\n".encode('utf-8'))
+                    current_status = s.recv(1024).decode('utf-8')
+        
+                print("Brakes disengaged; status is ", response)
+            else:
+                print("Assuming brakes are already disengaged; status is ", current_status)
+            
+            time.sleep(5)
 
             print("powering off and repeating this cycle")
             s.sendall("power off\n".encode('utf-8'))
-            time.sleep(1)
 
-            
+            while current_status.find("POWER_OFF") == -1:
+                s.sendall("robotmode\n".encode('utf-8'))
+                current_status = s.recv(1024).decode('utf-8')            
 
 except Exception as e:
     print(f"Error: {e}")
