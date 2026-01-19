f0b5520123d66b1efc0373bbf636428774a8ee31
Sheil Sarda <sheilsarda@yahoo.com>
2026-01-18 16:13:12 -0800

    Have a version that plans paths from current to any point in the workspace

--- path_planner.py
+++ path_planner.py
@@ -1,21 +1,23 @@
-import ikpy
+from ikpy.chain import Chain
 
-def plan_trajectory(pose_start, pose_end, n_waypoints=50):
-    """Generate collision-checkable trajectory."""
+class PathPlanner:
+    def __init__(self):
+        self.chain = Chain.from_urdf_file("universalUR5e.urdf")
+
+    def plan_trajectory(self, start_position, target_position, target_orientation, n_waypoints=50):
+        """Generate collision-checkable trajectory."""
+
+        q_end = self.chain.inverse_kinematics(target_position, target_orientation) # returns list of the positions of each joint according to the target, including inactive joints
+        
+        # Linear interpolation in joint space
+        trajectory = []
+        for i in range(n_waypoints):
+            alpha = i / (n_waypoints - 1)
+            q_waypoint = [(1 - alpha) * start_position[joint] + alpha * q_end[joint] for joint in range(len(start_position))]
+            trajectory.append(q_waypoint)
+        return trajectory
     
-    chain = ikpy.chain.Chain.from_urdf_file("universalUR5e.urdf")
-    # Get IK solutions at endpoints
-    q_start_options = chain.inverse_kinematics(pose_start)
-    q_end_options = chain.inverse_kinematics(pose_end)
-    
-    # Pick closest/smoothest solution pair (avoid big jumps)
-    q_start, q_end = ikpy.utils.closest_pair(q_start_options, q_end_options)
-    
-    # Linear interpolation in joint space
-    trajectory = []
-    for i in range(n_waypoints):
-        alpha = i / (n_waypoints - 1)
-        q = (1 - alpha) * q_start + alpha * q_end
-        trajectory.append(q)
-    
-    return trajectory
+    def check_trajectory_against_workspace_obstacles(self, trajectory, start_position, start_rail_position, obstacles):
+        """
+        TODO: Use Pybullet or some other sim to check if the trajectory shows robot is in collision with any workspace obstacles.
+        """
--- robot_controller.py
+++ robot_controller.py
@@ -1,18 +1,34 @@
 from csv import reader
 import numpy as np
 from robot_initializer import RobotInitializer
-from robot_comms_for_ur_sim import *
+from path_planner import PathPlanner
+from time import sleep
+# If we want to run this in sim, then use `robot_comms_for_ur_sim.py`, else use stubbed functions in `robot_communication.py`
+# from robot_comms_for_ur_sim import *
+from robot_communication import * 
 
-class WorkspaceObstacle:
+DEFAULT_HEIGHT = 500
+DEFUALT_DEPTH_FROM_RAIL = 500
+ROBOT_MODULE_WIDTH_ON_RAIL = 500
 
-    def __init__(self, obstacle_file_list: str, grex_location_file_list: str):
+class RobotController:
 
-        self.grex_location_list = []
+    def __init__(self):
+
+        self.grex_location_dict = {}
 
         self.load_grex_locations("grex_location_data.csv")
         self.load_rail_positions("module_data.csv")
         self.robot_initializer = RobotInitializer()
         self.robot_initializer.create_socket_and_initialize_robot()
+        self.path_planner = PathPlanner()
+
+        self.workspace_obstacles_to_consider_per_target = {
+            'Incubator': ['Add Media', 'Output for Scientist'],
+            'Output for Scientist': ['Incubator', 'Sample'],
+            'Sample': ['Output for Scientist', 'Incubator'], # 'Sample' and 'Add Media' are the same position, so we don't consider the other one in obstacle checks when planning a move to either
+            'Add Media': ['Output for Scientist', 'Incubator'],
+        }
 
     def load_grex_locations(self, grex_location_file_list: str) -> None:
         """
@@ -26,13 +42,14 @@ class WorkspaceObstacle:
                 position = [float(row[1]), float(row[2]), float(row[3])]
                 euler_angles_deg = [float(row[4]), float(row[5]), float(row[6])]
 
-                self.grex_location_list.append({
-                    'name': grex_location_name,
+                self.grex_location_dict[grex_location_name] = {
                     'position': position,
                     'euler_angles': euler_angles_deg,
                     'rail_position': -1.0, # shall populate from `module_data.csv`
                     'module_width': -1.0, # shall populate from `module_data.csv`
-                })
+                    'height': DEFAULT_HEIGHT,
+                    'depth_from_rail': DEFUALT_DEPTH_FROM_RAIL,
+                }
 
     def load_rail_positions(self, module_data_file_list: str) -> None:
         """
@@ -43,26 +60,35 @@ class WorkspaceObstacle:
             next(r)  # Skip header row
             for row in r:
                 obstacle_name = row[0]
+                if obstacle_name not in self.grex_location_dict:
+                    print(f"Note: Grex location {obstacle_name} not found")
+                    continue
                 rail_position = float(row[1] if row[1] != '-' else 0)
                 module_width = float(row[2] if row[2] != '-' else 0)
-                grex_to_update = next((x for x in self.grex_location_list if x['name'] == obstacle_name), None)
-                if grex_to_update is None:
-                    print(f"Error: Grex location {obstacle_name} not found")
-                    continue
-                grex_to_update['rail_position'] = rail_position
-                grex_to_update['module_width'] = module_width
+                self.grex_location_dict[obstacle_name]['rail_position'] = rail_position
+                self.grex_location_dict[obstacle_name]['module_width'] = module_width
 
 
 if __name__ == "__main__":
-    wc = WorkspaceObstacle("module_data.csv", "grex_location_data.csv")
-    
+    controller = RobotController()
+
     while True:
-        for target in wc.grex_location_list:
-            move_to_base_cartesian_position(
-                target['position'][0], target['position'][1], target['position'][2], 
-                target['euler_angles'][0], target['euler_angles'][1], target['euler_angles'][2])
-            move_to_rail_pose(target['rail_position'])
+        for target_name, target_info in controller.grex_location_dict.items():
+            current_joint_position = get_angular_pose()
+            trajectory = controller.path_planner.plan_trajectory(
+                current_joint_position, target_info['position'], target_info['euler_angles'])
             
-            print(f"Commanded move to {target['name']}: {target['position']} | euler angles {target['euler_angles']} | rail position {target['rail_position']} | module width {target['module_width']}")
-            time.sleep(5)
+            current_rail_bounds = [get_rail_pose() - ROBOT_MODULE_WIDTH_ON_RAIL/2, get_rail_pose() + ROBOT_MODULE_WIDTH_ON_RAIL/2]
+            obstacles_to_consider = []
+            for name_of_obstacle in controller.workspace_obstacles_to_consider_per_target[target_name]:
+                obstacles_to_consider.append(controller.grex_location_dict[name_of_obstacle])
+
+            controller.path_planner.check_trajectory_against_workspace_obstacles(
+                trajectory,
+                current_joint_position,
+                current_rail_bounds,
+                obstacles_to_consider)
+            
+            print(f"Computed waypoints for move from {current_joint_position} to {target_name}: {target_info['position']} | euler angles {target_info['euler_angles']} | rail position {target_info['rail_position']} | module width {target_info['module_width']}")
+            sleep(5)
             
